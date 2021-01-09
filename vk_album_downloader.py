b9f3b895b1e5466fdfb47c28ed5b73bc1a43b00f
Alexander Makeenkov <amakeenk@altlinux.org>
2021-01-09 16:33:36 +0300

    fix getting album name

--- vk_album_downloader.py
+++ vk_album_downloader.py
@@ -7,6 +7,7 @@ import math
 import os
 import requests
 import sys
+import time
 import vk
 
 
@@ -19,17 +20,23 @@ class vk_album_downloader():
         self.owner_id = url.split('/')[-1].split('_')[0].replace('album', '')
         self.album_id = url.split('/')[-1].split('_')[1]
         self.outdir_name = 'download'
-        self.album_dir_name = f'{self.outdir_name}/album_{self.owner_id}-{self.album_id}'
 
     def init_session(self):
         session = vk.Session(access_token=self.token)
         self.vk_api = vk.API(session, v=self.vk_api_version)
 
     def create_dirs(self):
+        self.album_dir_name = f'{self.get_album_name()}'
         if not os.path.exists(self.outdir_name):
             os.mkdir(self.outdir_name)
-        if not os.path.exists(self.album_dir_name):
-            os.mkdir(self.album_dir_name)
+        if not os.path.exists(f'{self.outdir_name}/{self.album_dir_name}'):
+            self.full_output_path = f'{self.outdir_name}/{self.album_dir_name}'
+        else:
+            self.full_output_path = f'{self.outdir_name}/{self.album_dir_name}-{time.time()}'
+        os.mkdir(self.full_output_path)
+
+    def get_album_name(self):
+        return self.vk_api.photos.getAlbums(owner_id=self.owner_id, album_ids=self.album_id)['items'][0]['title']
 
     def get_images_count(self):
         return self.vk_api.photos.getAlbums(owner_id=self.owner_id, album_ids=self.album_id)['items'][0]['size']
@@ -52,7 +59,7 @@ class vk_album_downloader():
                 image_url = image[f'photo_{best_quality}']
                 print(f'Downloading image {counter}/{images_count} with quality: {best_quality} [ progress: {progress}%]')
                 response = requests.get(image_url)
-                output_file = f'{self.album_dir_name}/image_{counter}.jpg'
+                output_file = f'{self.full_output_path}/image_{counter}.jpg'
                 try:
                     with open(output_file, 'wb') as file:
                         for data in response.iter_content(1024):
@@ -73,7 +80,7 @@ def main():
     downloader.init_session()
     downloader.create_dirs()
     downloader.download()
-    print(f'All done. Images dowloading to {downloader.album_dir_name}')
+    print(f'All done. Images dowloading to ./{downloader.full_output_path}')
 
 
 if __name__ == '__main__':
